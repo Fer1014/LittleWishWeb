5162c2cc78115ab8d1f8645c81e821d2c1f7830f
JhonnatanPajuelo <83382553+JhonnatanPajuelo@users.noreply.github.com>
2023-09-05 15:23:22 -0500

    Update Administrador.java
    Actualizacion de clase Administrador:
     Añadi Contructurores y getter and setter

--- Backend/LittleWishWeb/src/main/java/pe/edu/upc/aaw/littlewishproject/entities/Administrador.java
+++ Backend/LittleWishWeb/src/main/java/pe/edu/upc/aaw/littlewishproject/entities/Administrador.java
@@ -22,4 +22,82 @@ public class Administrador {
     private String Password_Admin;
     @Column(name = "FechaRegistro_Admin", nullable = false)
     private LocalDate FechaRegistro_Admin;
+
+    public Administrador() {
+    }
+
+    public Administrador(int id_Admin, String nombre_Admin, String apellido_Admin, String correo_Admin, int telefono_Admin, String username_Admin, String password_Admin, LocalDate fechaRegistro_Admin) {
+        Id_Admin = id_Admin;
+        Nombre_Admin = nombre_Admin;
+        Apellido_Admin = apellido_Admin;
+        Correo_Admin = correo_Admin;
+        Telefono_Admin = telefono_Admin;
+        Username_Admin = username_Admin;
+        Password_Admin = password_Admin;
+        FechaRegistro_Admin = fechaRegistro_Admin;
+    }
+
+    public int getId_Admin() {
+        return Id_Admin;
+    }
+
+    public void setId_Admin(int id_Admin) {
+        Id_Admin = id_Admin;
+    }
+
+    public String getNombre_Admin() {
+        return Nombre_Admin;
+    }
+
+    public void setNombre_Admin(String nombre_Admin) {
+        Nombre_Admin = nombre_Admin;
+    }
+
+    public String getApellido_Admin() {
+        return Apellido_Admin;
+    }
+
+    public void setApellido_Admin(String apellido_Admin) {
+        Apellido_Admin = apellido_Admin;
+    }
+
+    public String getCorreo_Admin() {
+        return Correo_Admin;
+    }
+
+    public void setCorreo_Admin(String correo_Admin) {
+        Correo_Admin = correo_Admin;
+    }
+
+    public int getTelefono_Admin() {
+        return Telefono_Admin;
+    }
+
+    public void setTelefono_Admin(int telefono_Admin) {
+        Telefono_Admin = telefono_Admin;
+    }
+
+    public String getUsername_Admin() {
+        return Username_Admin;
+    }
+
+    public void setUsername_Admin(String username_Admin) {
+        Username_Admin = username_Admin;
+    }
+
+    public String getPassword_Admin() {
+        return Password_Admin;
+    }
+
+    public void setPassword_Admin(String password_Admin) {
+        Password_Admin = password_Admin;
+    }
+
+    public LocalDate getFechaRegistro_Admin() {
+        return FechaRegistro_Admin;
+    }
+
+    public void setFechaRegistro_Admin(LocalDate fechaRegistro_Admin) {
+        FechaRegistro_Admin = fechaRegistro_Admin;
+    }
 }
