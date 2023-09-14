7a5f02fb6a413dcbf8e26b25b4a5931659be0682
Jhonnatan <83382553+JhonnatanPajuelo@users.noreply.github.com>
2023-09-14 13:41:10 -0500

    Creacion Pago,Proyecto,Tarjeta
    Se creo las entidades y se implemento el insertar y listar

---
+++ Backend/LittleWishWeb/src/main/java/pe/edu/upc/aaw/littlewishproject/dtos/ProyectoDTO.java
@@ -0,0 +1,56 @@
+package pe.edu.upc.aaw.littlewishproject.dtos;
+
+import java.time.LocalDate;
+
+public class ProyectoDTO {
+
+    private int idProyecto;
+
+    private String descripcion;
+
+
+    private LocalDate fechaInicio;
+
+    private LocalDate fechaFin;
+
+
+
+    //private Puntuacion puntuacion;
+
+
+    //private Solicitud solicitud;
+
+    //private Comentario comentario;
+
+    public int getIdProyecto() {
+        return idProyecto;
+    }
+
+    public void setIdProyecto(int idProyecto) {
+        this.idProyecto = idProyecto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+}
