463c66c98493a67036bb2f2a4c5a32774985e8c9
Geyler Edinson Ochoa Alarcon <143047852+0311031410040803@users.noreply.github.com>
2023-09-18 17:41:59 -0500

    CV_GEYLER OCHOA
    Se registraron nuevas tablas.

---
+++ demo2_sv64/Repositories/WorkExperienceRepository.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.aaw.demo2_sv64.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.upc.aaw.demo2_sv64.Entities.WorkExperience;
+
+public interface WorkExperienceRepository extends JpaRepository<WorkExperience,Integer> {
+}
